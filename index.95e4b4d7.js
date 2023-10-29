9543b8b0e3c4626e3bd8fc4f66bbde4691d87a1f
Razvan49 <128290757+Razvan49@users.noreply.github.com>
2023-10-29 09:32:23 +0000

    Deploying to gh-pages from @ Razvan49/Filmoteka@1d8852fb73fbc83727d5bc7d98799d6f503a59b9 🚀

---
+++ index.95e4b4d7.js
@@ -0,0 +1,2 @@
+!function(){var e={closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};e.closeModalBtn.addEventListener("click",(function(){e.modal.classList.toggle("gallery-is-hidden")}))}();
+//# sourceMappingURL=index.95e4b4d7.js.map
